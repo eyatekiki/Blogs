ca699a76f1d8aab586ceb4d58db5f5a4ddfcb21a
houssem <houssemfraj5@gmail.com>
2022-12-06 17:21:51 +0000

    hash password bcrypt

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,10 @@ import postRoutes from "./routes/posts.js"
 const app = express();
 
 
-app.use(express.json());
+
+app.use(express.json()); 
+
+
 app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
 app.use("/api/posts",postRoutes)
