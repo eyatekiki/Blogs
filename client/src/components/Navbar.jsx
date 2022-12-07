92d781a660595b7a1c33cf9688a0e165cafa75f5
unknown <zayatsunami@gmail.com>
2022-12-07 10:26:32 +0000

    navbar register.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import Logo from "../img/logo.png"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="container">
+        <div className="logo">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="links">
+          <Link className="link" to="/?cat=mother">
+            <h6>MOTHER</h6>
+            </Link>
+            <Link className="link" to="/?cat=child">
+            <h6>CHILD</h6>
+            </Link>
+            <Link className="link" to="/?cat=nutrition">
+            <h6>NUTRITION</h6>
+            </Link>
+            <span>John</span>
+            <span>Logout</span>
+            <span className="write">
+              <Link className="link" to="/write">Write</Link>
+            </span>
+            
+        </div>
+      </div>
+      </div>
   )
 }
 
